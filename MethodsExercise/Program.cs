86dd7e7c5fa5fc2179157cd55da6bfef27580a52
David Armstrong <wafflesultimate1@gmail.com>
2023-09-28 19:19:54 -0700

    Completed Exercises 1 and 2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,8 +1,11 @@
 ﻿namespace MethodsExercise
 {
+
+
     public class Program
     {
-        static void Main(string[] args)
+
+        static public void greeting()
         {
 
             Console.WriteLine("Hello, what is you name?");
@@ -21,10 +24,108 @@
 
             string userFavoriteAnimal = Console.ReadLine();
 
+
             Console.WriteLine($"Here is a short story for you:");
-            Console.WriteLine($"One day, {userName} went to the zoo. He saw {userFavoriteColor}, {userFavoriteAnimal} practicing with the {userFavoriteBand} band.");
+            Console.WriteLine($"One day, {userName} went to the zoo. He saw {userFavoriteColor}, {userFavoriteAnimal} practicing with the {userFavoriteBand}.");
+        }
+        static public int Sum(params int[] list)
+        {
+            int sum = 0;
+            for(int i = 0; i < list.Length; i++) 
+            {
+                sum = sum + list[i];
+            }
+            return sum;
+        }
 
+        static public int subtract(int x, int y)
+        {
+            return x - y;
+        }
 
+        static public int multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        static public double divide(double x, double y)
+        {
+            return x / y;
+        }
+
+        static public void calculater()
+        {
+            bool userExit = false;
+            do 
+            {
+                Console.WriteLine("What do you want to do now? You can add, subtract, multiply or divide?");
+                Console.WriteLine("To add type 1. To subtract type 2. To multiply type 3. To divide type 4. To exit type any number.");
+                int userInput = int.Parse(Console.ReadLine());
+                
+
+                if (userInput == 1)
+                {
+                    Console.WriteLine("Give me a number for addition.");
+                    int num1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Give me a second number for addition.");
+                    int num2 = int.Parse(Console.ReadLine());
+
+                    var result = Sum(num1, num2); 
+                    Console.WriteLine($"The result is: {result}");
+                     userExit = true;
+                }
+
+                else if (userInput == 2)
+                {
+                    Console.WriteLine("Give me a number for subtraction.");
+                    int num1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Give me a second number for subtraction.");
+                    int num2 = int.Parse(Console.ReadLine());
+
+                    var result = subtract(num1, num2);
+                    Console.WriteLine($"The result is: {result}");
+                    userExit = true;
+                }
+
+                else if (userInput == 3)
+                {
+                    Console.WriteLine("Give me a number for multiplication.");
+                    int num1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Give me a second number for multiplication.");
+                    int num2 = int.Parse(Console.ReadLine());
+
+                    var result = multiply(num1, num2);
+                    Console.WriteLine($"The result is: {result}");
+                    userExit = true;
+                }
+                else if (userInput == 4)
+                {
+                    Console.WriteLine("Give me a number for division.");
+                    int num1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Give me a second number for division.");
+                    int num2 = int.Parse(Console.ReadLine());
+
+                    var result = divide(num1, num2);
+                    Console.WriteLine($"The result is: {result}");
+                    userExit = true;
+                }
+                else
+                {
+                    userExit = false;
+                }
+            } while (userExit);
+
+        }
+
+        static void Main(string[] args)
+        {
+            greeting();
+
+            calculater();
         }
     }
 }
